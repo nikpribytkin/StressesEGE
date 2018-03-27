4a4f0f001d63fc5b5473d912ad89368c9d9c68d7
nikpribytkin <nikpribytkin@gmail.com>
2018-03-27 15:05:48 +0300

    Initial Commit

---
+++ StressesEGE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StressesEGE
+//
+//  Created by Nikita Pribytkin on 27.03.2018.
+//  Copyright © 2018 Nikita Pribytkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
